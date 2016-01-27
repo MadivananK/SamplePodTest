bee35c1a7796aff872131793feec8b22fd7b1023
MADIVANAN_K <madivanan_k@madivanan-k.local>
2016-01-27 17:05:53 +0530

    Commit

---
+++ Example/Pods/Target Support Files/SamplePodTest/SamplePodTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SamplePodTest : NSObject
+@end
+@implementation PodsDummy_SamplePodTest
+@end
---
+++ Example/Pods/Target Support Files/SamplePodTest/SamplePodTest-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double SamplePodTestVersionNumber;
+FOUNDATION_EXPORT const unsigned char SamplePodTestVersionString[];
+
